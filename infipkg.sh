15ff47837ee3f1da76a070f568475609f85f4fb2
PizzaLovingNerd <pizzalovingnerd@teknik.io>
2018-12-19 22:44:14 -0800

    Update infipkg.sh

--- infipkg.sh
+++ infipkg.sh
@@ -1,5 +1,8 @@
 if command -v nodejs >/dev/null 2>&1 ; then
-    # INSERT CODE HERE
+    if [ "$1" == "install" ]; then
+        git clone https://github.com/infipkg/infipkg$2.git
+        bash infipkg-$2/install.sh
+        rm -rf infipkg-$2
 else
     echo "Error: git not found... git is a dependency of infipkg"
 fi
